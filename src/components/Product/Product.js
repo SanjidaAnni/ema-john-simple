264e19d95c70321e58b006b0b56ea53414ffad1e
Sanjida Anni <sanjida.anni2@gmailcom>
2021-02-27 18:36:01 +0600

    done

---
+++ src/components/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Product = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h3>This is Product</h3>
+        </div>
+    );
+};
+
+export default Product;
